a0bdd431048406359b4098877a610607dce2c10d
veredsadde2498-cmyk <veredsadde2498@gmail.com>
2026-08-14 15:59:54 +0300

    chore: remove unused post.js and legacy routes/index.js

--- server/routes/index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const Post = require('../controllers/post'); // מייבא את הקונטרולר שפתחנו בשלב הקודם
-
-router.get('/', Post.getPosts);
-
-module.exports = router;
